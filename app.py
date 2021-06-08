30982753696e6eeb6a462a343c030c699696b236
M.A.Shanawaz <58618875+Shanawaz444@users.noreply.github.com>
2021-06-09 03:04:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 #inmporting all the essential libraries
 from flask import Flask
 from flask_restful import Resource,Api
-from flask_cors import CORS
-import numpy as np      # the numpy library is for numpy arrays
-import pandas as pd     # pandas is for preprossing the CSV files and converting into dataframes
-import matplotlib.pyplot as plt  # matplotlib is for visualising the data
-import seaborn as sns
 
 
 
@@ -16,8 +11,6 @@ api=Api(app)
 
 
 
-
-from sklearn.model_selection import train_test_split
 
 
 
